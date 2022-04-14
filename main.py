e03f890c1c9704474688236198e20cad281dde68
Emma Bates <100385290+ebates1977@users.noreply.github.com>
2022-04-14 20:56:12 +0000

    actually finished now (first time used a loop)

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import random
 
 range = int(input("enter the range you would like to play in (starts at 1): "))
+ran = random.randint(1, range)
+
+
+def random_number(range, ran):
+  guess = int(input("Enter an integer from 1 to " + str(range) + ": "))
+  
+  if guess > ran:
+    print( "guess is high")
+    random_number(range, ran)
+    
+  elif guess < ran:
+    print ("guess is low")
+    random_number(range, ran)
+    
+  else:
+    print ("you guessed it!")
+    play_again()
 
 def play_again():
   answer = input("do you want to play again? ")
-  if answer == "y" or answer == "Y" or answer == "Yes":
+  if answer == "y" or answer == "Y" or answer == "Yes" or answer == "yes":
     r = int(input("enter the range you would like to play in (starts at 1): "))
-    random_number(r)
+    rand = random.randint(1, range)
+    random_number(r, rand)
   else:
     print("okay have a good day!")
     exit()
     
-def random_number(range):
-  random_number = random.randint(1, range)
-  guess = int(input("Enter an integer from 1 to " + str(range) + ": "))
-  while random_number != "guess":
-      print
-      if guess < random_number:
-          print( "guess is low")
-          guess = int(input("Enter an integer from 1 to " + str(range) + ": "))
-      elif guess > random_number:
-          print ("guess is high")
-          guess = int(input("Enter an integer from 1 to " + str(range) + ": "))
-      else:
-          print ("you guessed it!")
-          play_again()
-          
-      print
-
-random_number(range)
+random_number(range, ran)
